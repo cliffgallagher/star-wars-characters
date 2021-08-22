176e3f62b0241297a81c7f120acd29764bd08579
cliffgallagher <scliffordgallagher@gmail.com>
2021-08-22 08:07:43 -0500

    search field clears after submit

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchForm = (props) => {
+  const [characterName, setCharacterName] = useState("");
+
+  const characterNameChangeHandler = (event) => {
+    setCharacterName(event.target.value)
+  }
+
   const submitFormHandler = (event) => {
     event.preventDefault();
     props.passSearchTerm(
-      document.querySelector("input[name='characterName']").value
+      characterName
     );
+    setCharacterName("");
   };
 
   return (
     <form onSubmit={submitFormHandler}>
       <label>
-        Character name:<input type="text" name="characterName"></input>
+        Character name:<input type="text" name="characterName" value={characterName} onChange={characterNameChangeHandler}></input>
       </label>
       <button type="submit">Search</button>
     </form>
